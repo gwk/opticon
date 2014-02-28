3110b367de09a57b568b0b6cb82450bf6b179ef6
George King <george.w.king@gmail.com>
2014-02-28 12:04:36 -0500

    event monitoring to NSLog; opticon-askpass works.

---
+++ src-app/AppDelegate.mm
@@ -0,0 +1,138 @@
+// Copyright 2014 George King.
+// Permission to use this file is granted in opticon/license.txt.
+
+
+#import "AppDelegate.h"
+
+
+AppDelegate* appDelegate;
+
+
+@interface AppDelegate ()
+
+@property (nonatomic) FILE* logFile;
+
+@end
+
+
+@implementation AppDelegate
+
+
+CGEventRef eventTapped(CGEventTapProxy proxy, CGEventType type, CGEventRef event, void* ctx) {
+  // event tap callback.
+  switch (type) {
+    case kCGEventMouseMoved: break;
+    case kCGEventLeftMouseDragged: break;
+    case kCGEventRightMouseDragged: break;
+    case kCGEventNull:
+    case kCGEventLeftMouseDown:
+    case kCGEventLeftMouseUp:
+    case kCGEventRightMouseDown:
+    case kCGEventRightMouseUp:
+    case kCGEventKeyDown:
+    case kCGEventKeyUp:
+    case kCGEventFlagsChanged:
+    case kCGEventScrollWheel:
+    case kCGEventTabletPointer:
+    case kCGEventTabletProximity:
+    case kCGEventOtherMouseDown:
+    case kCGEventOtherMouseUp:
+    case kCGEventOtherMouseDragged:
+      NSLog(@"event: %d", type);
+  }
+  return NULL;
+}
+
+
+- (void)workspaceNote:(NSNotification*)note {
+  // workspace notification callback.
+  NSRunningApplication* app = note.userInfo[NSWorkspaceApplicationKey];
+  NSLog(@"note: %@: %@", note.name, app.bundleIdentifier);
+}
+
+
+- (void)observeValueForKeyPath:(NSString*)keyPath
+                      ofObject:(id)object
+                        change:(NSDictionary*)change
+                       context:(void *)context {
+  if ([keyPath isEqualToString:@"frontmostApplication"]) {
+    NSLog(@"frontmostApplication %@", change);
+  }
+  else if ([keyPath isEqualToString:@"menuBarOwningApplication"]) {
+    NSLog(@"menuBarOwningApplication %@", change);
+  }
+  else {
+    NSLog(@"unknown observation: %@", keyPath);
+  }
+}
+
+
+- (void)addNote:(NSString*)name {
+  auto wsnc = [[NSWorkspace sharedWorkspace] notificationCenter];
+  [wsnc addObserver:self selector:@selector(workspaceNote:) name:name object:nil];
+}
+
+
+- (void)setupMonitors {
+
+  [self addNote:NSWorkspaceWillLaunchApplicationNotification];
+  [self addNote:NSWorkspaceDidLaunchApplicationNotification];
+  [self addNote:NSWorkspaceDidTerminateApplicationNotification];
+  [self addNote:NSWorkspaceDidHideApplicationNotification];
+  [self addNote:NSWorkspaceDidUnhideApplicationNotification];
+  [self addNote:NSWorkspaceDidActivateApplicationNotification];
+  [self addNote:NSWorkspaceDidDeactivateApplicationNotification];
+
+  auto opts = NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld;
+  [[NSWorkspace sharedWorkspace] addObserver:self forKeyPath:@"frontmostApplication" options:opts context:NULL];
+  [[NSWorkspace sharedWorkspace] addObserver:self forKeyPath:@"menuBarOwningApplication" options:opts context:NULL];
+
+  CGEventMask eventMask = 0
+  | CGEventMaskBit(kCGEventLeftMouseDown)
+  | CGEventMaskBit(kCGEventLeftMouseUp)
+  | CGEventMaskBit(kCGEventRightMouseDown)
+  | CGEventMaskBit(kCGEventRightMouseUp)
+  | CGEventMaskBit(kCGEventMouseMoved)
+  | CGEventMaskBit(kCGEventLeftMouseDragged)
+  | CGEventMaskBit(kCGEventRightMouseDragged)
+  | CGEventMaskBit(kCGEventKeyDown)
+  | CGEventMaskBit(kCGEventKeyUp)
+  | CGEventMaskBit(kCGEventFlagsChanged)
+  | CGEventMaskBit(kCGEventScrollWheel)
+  | CGEventMaskBit(kCGEventTabletPointer)
+  | CGEventMaskBit(kCGEventTabletProximity)
+  | CGEventMaskBit(kCGEventOtherMouseDown)
+  | CGEventMaskBit(kCGEventOtherMouseUp)
+  | CGEventMaskBit(kCGEventOtherMouseDragged)
+  ;
+  
+  CFMachPortRef tap =
+  CGEventTapCreate(kCGAnnotatedSessionEventTap, // tap events as they flow into applications (as late as possible).
+                   kCGTailAppendEventTap, // insert tap after any existing filters.
+                   kCGEventTapOptionListenOnly, // passive tap..
+                   eventMask,
+                   eventTapped,
+                   NULL);
+  
+  NSLog(@"tap: %p; enabled: %d", tap, CGEventTapIsEnabled(tap));
+  
+  CFRunLoopSourceRef source = CFMachPortCreateRunLoopSource(NULL, tap, 0);
+  CFRunLoopAddSource([[NSRunLoop currentRunLoop] getCFRunLoop], source, kCFRunLoopCommonModes);
+  CFRelease(source);
+  
+  // key down/up events are only delivered if the current process is running as root,
+  // or the process has been approved as trusted for accessibility.
+  // this is set in System Preferences -> Security and Privacy -> Privacy -> Accessibility.
+  auto options = @{(__bridge id)kAXTrustedCheckOptionPrompt: @YES};
+  BOOL isTrusted = AXIsProcessTrustedWithOptions((__bridge CFDictionaryRef)options);
+  NSLog(@"accessibility trusted: %d", isTrusted);
+}
+
+
+
+- (void)applicationDidFinishLaunching:(NSNotification*)note {
+  [self setupMonitors];
+}
+
+
+@end
---
+++ src-askpass/opticon-askpass.c
@@ -0,0 +1,72 @@
+// Copyright 2014 George King.
+// Permission to use this file is granted in opticon/license.txt.
+
+// program to serve as SUDO_ASKPASS helper.
+// this uses carbon's secure text entry mechanism to prevent your user password from being
+// logged by opticon, or any other event tap.
+
+// to use this, add the following to your .bashrc / .bash_profile:
+//  export SUDO_ASKPASS=~/bin/opticon-askpass # or whatever install path you choose.
+//  alias sudo='sudo -A'
+
+
+#import <stdio.h>
+#import <signal.h>
+#import <termios.h>
+#import <Carbon/Carbon.h>
+
+#define errFL(msg, ...) fprintf(stderr, msg "\n", ## __VA_ARGS__)
+
+bool interrupted = false;
+
+void sig_handler(int sig) {
+  interrupted = true;
+}
+
+
+int main(int argc, const char * argv[]) {
+  // setup handler for SIGINT; we want to exit cleanly in this case so that sudo will not retry.
+  struct sigaction action;
+  sigemptyset(&action.sa_mask);
+  action.sa_handler = sig_handler;
+  sigaction(SIGINT, &action, NULL);
+  // print prompt.
+  fputs("※ ", stderr);
+  fflush(stderr);
+  // turn off echo.
+  struct termios std_state, silent_state;
+  int stdin_fdesc = fileno(stdin);
+  int code = tcgetattr(stdin_fdesc, &std_state);
+  if (code) return code;
+  silent_state = std_state;
+  silent_state.c_lflag &= ~ECHO;
+  code = tcsetattr(stdin_fdesc, TCSADRAIN, &silent_state);
+  if (code) return code;
+  // turn on secure input.
+  EnableSecureEventInput();
+  // read the entire password in, and only output once complete.
+  // this way if the user interrupts with ctrl-c, sudo receives an empty string and aborts,
+  // rather than retrying.
+  const int len_buffer = 256; // same as OSX 10.9 sudo-72 implementation.
+  char buffer[len_buffer];
+  int len = 0;
+  while (len < len_buffer) {
+    if (interrupted) {
+      goto reset;
+    }
+    char c = getchar();
+    if (c == EOF) {
+      goto reset;
+    }
+    buffer[len++] = c;
+    if (c == '\n') break;
+  }
+  fwrite(buffer, 1, len, stdout);
+  fflush(stdout);
+  // reset state.
+reset:
+  DisableSecureEventInput(); // unnecessary?
+  code = tcsetattr(stdin_fdesc, TCSADRAIN, &std_state);
+  return code;
+}
+
