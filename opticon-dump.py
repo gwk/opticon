6ef4f1b7c41a558a4d0c69a4659a47281e8dd9f0
George King <george.w.king@gmail.com>
2014-05-12 18:57:46 -0400

    opticon-dump shows more readable times.

--- opticon-dump.py
+++ opticon-dump.py
@@ -7,6 +7,8 @@ import sqlite3
 import struct
 import sys
 
+from datetime import datetime
+
 event_type_names = [
   'Unknown',
   'DisabledByUser',
@@ -54,8 +56,9 @@ c.execute('SELECT * FROM events')
 
 for row in c:
   idx, time, type_id, pid, flags, data = row
+  time_str = datetime.fromtimestamp(time)
   type_name = event_type_names[type_id]
-  print('{:06} time:{:010.2f} type:{:02} pid:{:05} flags:{:08X} {:16} '.format(idx, time, type_id, pid, flags, type_name), end='')
+  print('{:06} {} type:{:02} pid:{:05} flags:{:08X} {:16} '.format(idx, time_str, type_id, pid, flags, type_name), end='')
   if type_name == 'Mouse':
     print()
     for time, x, y, pressure_uns, event_num, bits in mouse_struct.iter_unpack(data):
@@ -65,7 +68,7 @@ for row in c:
       sub   = (bits >> 4) & 0b11
       down  = (bits >> 6) & 0b1
       move  = (bits >> 7) & 0b1
-      print('  time:{:010.2f} x:{:+05} y:{:+05} pressure:{:.2} ev:{:03} bttn:{:01} click:{:01} sub:{:01} down:{:01} move:{:01}'.format(
+      print('  t:{:.2f} x:{:+05} y:{:+05} pressure:{:.2} ev:{:03} bttn:{:01} click:{:01} sub:{:01} down:{:01} move:{:01}'.format(
         time, x, y, pressure, event_num, bttn, click, sub, down, move))
   elif type_name == 'Key':
     print()
@@ -74,11 +77,11 @@ for row in c:
       ar    = (bits >> 30) & 0b1
       down  = (bits >> 31) & 0b1
       char = chr(char_ord)
-      print('  time:{:010.2f} code:{:02X} char:{} kb:{:02X} ar:{:01} down:{:01}'.format(time, code, repr(char), kb, ar, down))
+      print('  t:{:.2f} code:{:02X} char:{} kb:{:02X} ar:{:01} down:{:01}'.format(time, code, repr(char), kb, ar, down))
   elif type_name == 'Wheel':
     print()
     for time, dx, dy in wheel_struct.iter_unpack(data):
-      print('  time:{:010.2f} dx:{:01} dy:{:01}'.format(time, dx, dy))
+      print('  t:{:.2f} dx:{:01} dy:{:01}'.format(time, dx, dy))
   elif data: # data column is utf8
     print(data.decode())
   else:
