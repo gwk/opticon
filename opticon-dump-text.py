dede49c28ded11ddbb0eb6a00a8d1a20296b3f46
George King <george.w.king@gmail.com>
2014-09-14 17:24:49 -0700

    dump scripts.

--- opticon-dump-text.py
+++ opticon-dump-text.py
@@ -65,31 +65,15 @@ for row in c:
   idx, time, type_id, pid, flags, data = row
   time_str = datetime.fromtimestamp(time)
   type_name = event_type_names[type_id]
+  if type_name != 'Key':
+    continue
   print('{:06} {} type:{:02} pid:{:05} flags:{:08X} {:16} '.format(idx, time_str, type_id, pid, flags, type_name), end='')
-  if type_name == 'Mouse':
-    print()
-    for time, x, y, pressure_uns, event_num, bits in mouse_struct.iter_unpack(data):
-      pressure = pressure_uns / pressure_max
-      bttn  = (bits >> 0) & 0b11
-      click = (bits >> 2) & 0b11
-      sub   = (bits >> 4) & 0b11
-      down  = (bits >> 6) & 0b1
-      move  = (bits >> 7) & 0b1
-      print('  t:{:.2f} x:{:+05} y:{:+05} pressure:{:.2} ev:{:03} bttn:{:01} click:{:01} sub:{:01} down:{:01} move:{:01}'.format(
-        time, x, y, pressure, event_num, bttn, click, sub, down, move))
-  elif type_name == 'Key':
-    print()
-    for time, code, char_ord, bits in key_struct.iter_unpack(data):
-      kb    = (bits >> 0) & 0x3FFFFFFF
-      ar    = (bits >> 30) & 0b1
-      down  = (bits >> 31) & 0b1
-      char = chr(char_ord)
-      print('  t:{:.2f} code:{:02X} char:{} kb:{:02X} ar:{:01} down:{:01}'.format(time, code, repr(char), kb, ar, down))
-  elif type_name == 'Wheel':
-    print()
-    for time, dx, dy in wheel_struct.iter_unpack(data):
-      print('  t:{:.2f} dx:{:01} dy:{:01}'.format(time, dx, dy))
-  elif data: # data column is utf8
-    print(data.decode())
-  else:
-    print()
+  chars = []
+  for time, code, char_ord, bits in key_struct.iter_unpack(data):
+    kb    = (bits >> 0) & 0x3FFFFFFF
+    ar    = (bits >> 30) & 0b1
+    down  = (bits >> 31) & 0b1
+    if not down: continue
+    char = chr(char_ord)
+    chars.append(char)
+  print(repr(''.join(chars)))
